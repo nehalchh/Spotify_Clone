41254c57048caafa448c4b19ae29bf658f6deeae
Nehalchh <nehalchhalotre27@gmail.com>
2024-07-04 05:04:59 +0530

    added songs

--- script.js
+++ script.js
@@ -17,6 +17,12 @@ async function main(){
     let songs= await getsongs();
     console.log(songs);
 
+    let songUL= document.querySelector("#toplay").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        songUL.innerHTML= songUL.innerHTML + song
+        
+    }
+
     var audio= new Audio(songs[0]);
     audio.play();
     audio.addEventListener("loadeddata", () =>{
